fe46c85bc8c3e2931bffa2253c68030906f89ee4
Tais993 <tijs@familiebeek.eu>
2023-11-17 14:07:22 +0100

    Combined the separate WiFi request file with the rest of the code

--- src/arduino_secrets.h
+++ src/arduino_secrets.h
@@ -1,3 +1,5 @@
 #define SECRET_SSID "Netlab-OIL"
 #define SECRET_PASS "DesignChallenge"
+#define SECRET_ADDRESS "waterkwaliteit.kaliflex.nl"
+#define SECRET_PORT 80
 //#define SECRET_SSID "12connect"
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,53 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include "Arduino.h"
+#include <ArduinoHttpClient.h>
+#include <WiFi.h>
+#include "arduino_secrets.h"
 
-
-const int GPSReceivePin = 4, GPSTransmitPin = 2, WaterLevelSensor = A5, POWER_PIN = 7;
+const int GPSReceivePin = 4, GPSTransmitPin = 2;
+const int WaterLevelSensor = A5, POWER_PIN = 7;
+const int TemperatureSensor = A1;
 
 const uint32_t GPSBaud = 9600; //Default baud of NEO-6M is 9600
 
 
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+char serverAddress[] = SECRET_ADDRESS;  // server address
+int port = SECRET_PORT;
+
+WiFiClient wifi;
+HttpClient client = HttpClient(wifi, serverAddress, port);
+int status = WL_IDLE_STATUS;
+
 TinyGPSPlus gps;
 SoftwareSerial gpsSerial(GPSReceivePin, GPSTransmitPin);
+
+void connectWithWifi() {
+    while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to Network named: ");
+        Serial.println(ssid);
+        // print the network name (SSID);
+
+        // Connect to WPA/WPA2 network:
+        status = WiFi.begin(ssid, pass);
+    }
+
+    Serial.println("Connected!");
+}
 
 void setup() {
     Serial.begin(9600);
     gpsSerial.begin(GPSBaud);
     pinMode(POWER_PIN, OUTPUT);
     digitalWrite(POWER_PIN, LOW);
+    connectWithWifi();
 }
 
 void checkLocation() {
+    TinyGPSLocation &location =gps.location;
+    
     if (gpsSerial.available() > 0) {
         if (gps.encode(gpsSerial.read())) {
             if (gps.location.isValid()) {
@@ -46,7 +75,7 @@ void checkWaterHeight() {
 }
 
 float checkTemperature() {
-    int sensorValue = analogRead(A0);
+    int sensorValue = analogRead(TemperatureSensor);
     float voltage = sensorValue * (5.0 / 1023.0);
     float temperature = voltage - 0.5;
     temperature = temperature / 0.01;
@@ -54,8 +83,34 @@ float checkTemperature() {
     return temperature - 10;
 }
 
+void sendRequest() {
+    String contentType = "application/json";
+    String postData = "{\n"
+                      "    \"tabel\": \"parameters\",\n"
+                      "    \"meetEenheid\": \"PH\",\n"
+                      "    \"naam\": \"zuurgraad5\" \n"
+                      "}";
+
+    client.post("/api/waterkwaliteit", contentType, postData);
+
+    // read the status code and body of the response
+    int statusCode = client.responseStatusCode();
+    String response = client.responseBody();
+
+    if (statusCode != 200) {
+        Serial.print("Status code: ");
+        Serial.println(statusCode);
+        Serial.print("Response: ");
+        Serial.println(response);
+    } else {
+        Serial.println("Successfully sent info");
+    }
+}
+
 void loop() {
     checkLocation();
     checkWaterHeight();
+    Serial.print("Temperature: ");
     Serial.println(checkTemperature());
+//    sendRequest();
 }
