117f0a9209859b9cfd8e14726ab282cfb7ef7c3e
Tais993 <tijs@familiebeek.eu>
2023-11-20 14:03:53 +0100

    Added PH sensor

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,14 @@ void setup() {
     connectWithWifi();
 }
 
+class PostRequestInfo{
+public:
+    String name;
+    String meetEenheid;
+
+};
+
+
 void checkLocation() {
     TinyGPSLocation &location =gps.location;
     
@@ -107,9 +115,100 @@ void sendRequest() {
     }
 }
 
+#include "Arduino.h"
+/*
+  # This sample code is used to test the pH meter V1.1.
+  # Editor : YouYou
+  # Date   : 2014.06.23
+  # Ver    : 1.1
+  # Product: analog pH meter
+  # SKU    : SEN0161
+*/
+#define SensorPin A2            //pH meter Analog output to Arduino Analog Input 0
+#define Offset 41.02740741      //deviation compensate
+#define LED 8
+#define samplingInterval 20
+#define printInterval 800
+#define ArrayLenth  40    //times of collection
+#define uart Serial
+int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
+int pHArrayIndex = 0;
+void setup(void)
+
+{
+    pinMode(LED, OUTPUT);
+    uart.begin(9600);
+    uart.println("pH meter experiment!");    //Test the uart monitor
+}
+
+double avergearray(int* arr, int number) {
+    int i;
+    int max, min;
+    double avg;
+    long amount = 0;
+    if (number <= 0) {
+        uart.println("Error number for the array to avraging!/n");
+        return 0;
+    }
+    if (number < 5) { //less than 5, calculated directly statistics
+        for (i = 0; i < number; i++) {
+            amount += arr[i];
+        }
+        avg = amount / number;
+        return avg;
+    } else {
+        if (arr[0] < arr[1]) {
+            min = arr[0]; max = arr[1];
+        }
+        else {
+            min = arr[1]; max = arr[0];
+        }
+        for (i = 2; i < number; i++) {
+            if (arr[i] < min) {
+                amount += min;      //arr<min
+                min = arr[i];
+            } else {
+                if (arr[i] > max) {
+                    amount += max;  //arr>max
+                    max = arr[i];
+                } else {
+                    amount += arr[i]; //min<=arr<=max
+                }
+            }//if
+        }//for
+        avg = (double)amount / (number - 2);
+    }//if
+    return avg;
+}
+
+void checkPh() {
+    static unsigned long samplingTime = millis();
+    static unsigned long printTime = millis();
+    static float pHValue, voltage;
+    if (millis() - samplingTime > samplingInterval)
+    {
+        pHArray[pHArrayIndex++] = analogRead(SensorPin);
+        if (pHArrayIndex == ArrayLenth)pHArrayIndex = 0;
+        voltage = avergearray(pHArray, ArrayLenth) * 5.0 / 1024;
+        pHValue = -19.18518519 * voltage + Offset;
+        samplingTime = millis();
+    }
+    if (millis() - printTime > printInterval)  //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+    {
+        uart.print("Voltage:");
+        uart.print(voltage, 2);
+        uart.print("    pH value: ");
+        uart.println(pHValue, 2);
+        digitalWrite(LED, digitalRead(LED) ^ 1);
+        printTime = millis();
+    }
+}
+
+
 void loop() {
     checkLocation();
     checkWaterHeight();
+    checkPh();
     Serial.print("Temperature: ");
     Serial.println(checkTemperature());
 //    sendRequest();
